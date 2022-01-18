58d4ff393bd4136123f680c5e7a3846d6f633fa4
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-18 18:44:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 import java.util.Scanner;
-
+//use 1 class for 1 task
+//did not find first task implementation with logic operations.
+//did not find 2.3 with if-else construction
+//did not find 4.2 with if-else construction
 public class Main{
     public static void main(String[] args) {
 
@@ -12,7 +15,7 @@ public class Main{
         System.out.println("Enter third number");
         int third = scanner.nextInt();
 
-
+//+
         if (first > second) {
             if (first > third) {
                 System.out.println(first);
@@ -34,7 +37,7 @@ public class Main{
 
 
         //task2.2
-
+//+
         System.out.println("Enter number one");
         int first1 = scanner.nextInt();
         System.out.println("Enter number two");
@@ -43,11 +46,12 @@ public class Main{
             System.out.println("Числа кратные");
 
         }
+        //in 38-43 you use english, but here it's russian. Looks strange:)
         else System.out.println("Числа не кратные");
 
 
         //task 3
-
+//+
         System.out.println("Enter number month");
         int month= scanner.nextInt();
 
@@ -76,7 +80,7 @@ public class Main{
                 System.out.println("404");
         }
 
-
+//+
         System.out.println("Enter day");
         int day= scanner.nextInt();
 
@@ -117,7 +121,8 @@ public class Main{
         System.out.println("3-Difference");
         System.out.println("4-Multiplication");
         int result = scanner.nextInt();
-
+        
+//better to write if (twoNumber == 0 ){sout("division by....")}else{//your logic} It just looks more beautiful, no difference in working logic
       if (twoNumber != 0) {
         switch (result){
                 
