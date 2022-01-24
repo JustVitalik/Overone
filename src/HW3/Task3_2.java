60e1b9175aa862feeb747d2335597e74449bcf98
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-24 18:36:53 +0300

    Update Task3_2.java

--- src/HW3/Task3_2.java
+++ src/HW3/Task3_2.java
@@ -1,15 +1,15 @@
 package HW3;
-
+//code formatting!!!
 public class Task3_2 {
     public static void main(String[] args) {
         //Напишите программу вывода чисел всех четных чисел от 2 до 100 включительно
         int i=0;
 
         while (i<=100){
-
+//empty line
             System.out.println(i);
             i=i+2;
-
+//empty line
         }
 
     }
