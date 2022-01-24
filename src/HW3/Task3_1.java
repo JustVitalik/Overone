f4f76a6127a2c88d0fb7a48edb9cbbabd0513943
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-24 18:35:56 +0300

    Update Task3_1.java

--- src/HW3/Task3_1.java
+++ src/HW3/Task3_1.java
@@ -1,16 +1,17 @@
 package HW3;
-
+//code formatting!!!!
 public class Task3_1 {
     public static void main(String[] args) {
         //Вычислить: 1+2+4+8+...+256
         int i=1;
         int result=0;
         while (i<=256){
+//result+=i, i*=2
             result=result+i;
             i=i*2;
 
         }System.out.println(result);
-
+//delete empty line
 
     }
 }
